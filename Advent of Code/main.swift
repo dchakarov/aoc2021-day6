ce5e8b02d3dff07e0d1056f9229f69eb173a99c4
Dimitar Chakarov <dimitar.chakarov@just-eat.com>
2021-12-07 09:53:28 +0000

    Part 2

--- Advent of Code/main.swift
+++ Advent of Code/main.swift
@@ -33,4 +33,32 @@ func main() {
     print(fishes.count)
 }
 
-main()
+func main2() {
+    let fileUrl = URL(fileURLWithPath: "./aoc-input")
+    guard let inputString = try? String(contentsOf: fileUrl, encoding: .utf8) else { fatalError("Invalid input") }
+    
+    let lines = inputString.components(separatedBy: "\n")
+        .filter { !$0.isEmpty }
+    
+    let initialFishes = lines[0].components(separatedBy: ",").map { Int($0)! }
+
+    var fishAges: [Int] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    for fishAge in initialFishes {
+        fishAges[fishAge] += 1
+    }
+    
+    for _ in 0 ..< 256 {
+        var newAges: [Int] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        let newFish = fishAges[0]
+        for i in 0 ..< 8 { newAges[i] = fishAges[i + 1] }
+        newAges[6] += newFish
+        newAges[8] = newFish
+        fishAges = newAges
+    }
+    
+    print(fishAges.reduce(0, +))
+}
+
+//main()
+main2()
+
